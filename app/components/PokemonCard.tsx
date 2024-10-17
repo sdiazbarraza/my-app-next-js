9478c3d4b005ffa16ad16a4bc0f05bff2fc233e9
sdiazbarraza <sebastiandesarrolla@gmail.com>
2024-10-17 03:04:27 +0000

    refactor: create component with call async

--- app/components/PokemonCard.tsx
+++ app/components/PokemonCard.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 import React from 'react';
 import Image from "next/image";
+import { getPokemonDetails } from "../utils/api/getPokemonDetails";
+
 
 interface PokemonCardProps {
   pokemon: {
@@ -14,26 +16,15 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-      const fetchPokemonData = async () => {
-        try {
-          const response = await fetch(pokemon.url);
-          if (!response.ok) {
-            throw new Error("failed to fetch");
-          }
-          const fetchedData =  await response.json();
-          setData(fetchedData);
-        } catch (error) {
-          console.log(error);
-          return null;
-        }
-      };
-  
-      fetchPokemonData();
-     
+    const fetchData = async () => {
+      const fetchedData = await getPokemonDetails(pokemon.url);
+      setData(fetchedData);
+    };
+    fetchData();
     }, [pokemon.url]);
   return (
     <li>
-      <a href={pokemon.url} target="_blank" rel="noopener noreferrer">
+      <a href="/details" target="_blank" rel="noopener noreferrer">
         {pokemon.name}
         <Image src={data?.sprites.front_default} alt={pokemon.name} width={100} height={100} />
       </a>
@@ -42,3 +33,4 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
 };
 
 export default PokemonCard;
+
