2edd31a9a2f4378601ec243551d702c79c7cd3c5
sdiazbarraza <sebastiandesarrolla@gmail.com>
2024-10-18 01:26:59 +0000

    feature: add attributes of the pokemon detail

--- app/pokemon/[name]/details/page.tsx
+++ app/pokemon/[name]/details/page.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-export default  function Details({
-  params: { name },
-}: {
-  params: { name: string }
-}) {
-  return <h1>Details page</h1>
+import { getPokemonDetails } from "@/app/utils/api/getPokemonDetails";
+import { useEffect, useState } from "react";
+
+interface PokemonParams {
+  params: {
+    name: string;    
+  };
+}
+
+export default function Details({ params }: PokemonParams) {
+  const [pokemon, setPokemon] = useState<any | null>(null);
+  const pokemonName = params.name;
+
+    useEffect(() => {
+      async function fetchData() {
+        let res = await getPokemonDetails(pokemonName);
+        setPokemon(res);
+      }
+      fetchData();   
+    }, [pokemonName]);
+
+  return <h1>Details page {pokemon?.name}</h1>
 }
 
 
